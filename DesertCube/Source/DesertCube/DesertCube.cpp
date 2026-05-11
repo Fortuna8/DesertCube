2def022e3d61541d70c1430594a9abfa92bbf8e2
Fortuna8 <ffmaglione@hotmail.com>
2026-05-11 13:51:14 -0300

    Commit 0 - Creación de proyecto en C++
    Creación de proyecto DesertCube en Unreal Engine 5.7.4
    
    Especificaciones de proyecto:
    * Blank
    * C++
    * Scalable

---
+++ DesertCube/Source/DesertCube/DesertCube.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DesertCube.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DesertCube, "DesertCube" );
